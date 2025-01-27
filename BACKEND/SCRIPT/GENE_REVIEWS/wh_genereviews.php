27daa7f5cd994f84d200d87f2f9f787d85ee1a2e
jdesaphy <desaphy_jeremy@lilly.com>
2025-01-27 15:13:48 -0500

    Add missing variables

--- BACKEND/SCRIPT/GENE_REVIEWS/wh_genereviews.php
+++ BACKEND/SCRIPT/GENE_REVIEWS/wh_genereviews.php
@@ -348,9 +348,13 @@ function preloadDataFromFiles(&$EXT_DATA,&$DATA)
 
 function compareRecords(&$CURR_INFO,&$INFO,&$DS_LIST,&$SCHEMA)
 {
+	global $JOB_ID;
+	global $EXT_DATA;
+	global $DB_CONN;
+	global $DISCLAIMER;
 
 	/// We are going to compare the current data with the new data
-
+	$DBID=$CURR_INFO['news_id'];
 	/// Setting the current data as valid
 	$CURR_INFO['DB_STATUS']='VALID';
 
